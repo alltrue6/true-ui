d6eb1bc44f5c0bb3bb10929513565e1c90c6f142
alltrue <10217099+alltrue6@user.noreply.gitee.com>
2022-10-27 16:41:42 +0800

    router

--- src/components/trueButton.vue
+++ src/components/trueButton.vue
@@ -2,14 +2,60 @@
 
 </script>
 <template>
-        <div class="flex flex wrap">
+    <div>
+        <!-- 基础用法 -->
+        <div class="flex flex wrap mt-10">
             <button class="trueButton trueButton--default">Default</button>
-            <!-- <button class="Primary">Primary</button>
-            <button class="Success">Success</button>
-            <button class="Info">Info</button>
-            <button class="Warning">Warning</button>
-            <button class="Danger">Danger</button> -->
+            <button class="trueButton trueButton--primary">Primary</button>
+            <button class="trueButton trueButton--success">Success</button>
+            <button class="trueButton trueButton--info">Info</button>
+            <button class="trueButton trueButton--warning">Warning</button>
+            <button class="trueButton trueButton--danger">Danger</button>
         </div>
+        <div class="flex flex wrap mt-10">
+            <button class="trueButton trueButton--default radius-min">Default</button>
+            <button class="trueButton trueButton--primary radius-min">Primary</button>
+            <button class="trueButton trueButton--success radius-min">Success</button>
+            <button class="trueButton trueButton--info radius-min">Info</button>
+            <button class="trueButton trueButton--warning radius-min">Warning</button>
+            <button class="trueButton trueButton--danger radius-min">Danger</button>
+        </div>
+        <div class="flex flex wrap mt-10">
+            <button class="trueButton trueButton--default radius-max">Default</button>
+            <button class="trueButton trueButton--primary radius-max">Primary</button>
+            <button class="trueButton trueButton--success radius-max">Success</button>
+            <button class="trueButton trueButton--info radius-max">Info</button>
+            <button class="trueButton trueButton--warning radius-max">Warning</button>
+            <button class="trueButton trueButton--danger radius-max">Danger</button>
+        </div>
+        <div class="flex flex wrap mt-10">
+            <button class="trueButton trueButton--default round_btn"></button>
+            <button class="trueButton trueButton--primary round_btn"></button>
+            <button class="trueButton trueButton--success round_btn"></button>
+            <button class="trueButton trueButton--info round_btn"></button>
+            <button class="trueButton trueButton--warning round_btn"></button>
+            <button class="trueButton trueButton--danger round_btn"></button>
+        </div>
+        <!-- 禁用状态 -->
+        <div class="flex flex wrap mt-10">
+            <button class="trueButton trueButton--default disable" disabled>Default</button>
+            <button class="trueButton trueButton--primary disable" disabled>Primary</button>
+            <button class="trueButton trueButton--success disable" disabled>Success</button>
+            <button class="trueButton trueButton--info disable" disabled>Info</button>
+            <button class="trueButton trueButton--warning disable" disabled>Warning</button>
+            <button class="trueButton trueButton--danger disable" disabled>Danger</button>
+        </div>
+        <div class="flex flex wrap mt-10">
+            <button class="trueButton trueButton--default radius-min disable" disabled>Default</button>
+            <button class="trueButton trueButton--primary radius-min disable" disabled>Primary</button>
+            <button class="trueButton trueButton--success radius-min disable" disabled>Success</button>
+            <button class="trueButton trueButton--info radius-min disable" disabled>Info</button>
+            <button class="trueButton trueButton--warning radius-min disable" disabled>Warning</button>
+            <button class="trueButton trueButton--danger radius-min disable" disabled>Danger</button>
+        </div>
+        <!-- 按钮组 -->
+    </div>
+    
 </template>
 <style scope lang='scss'>
 
