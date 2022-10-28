feb25ef91c20fde3029c109cdf0176ba4a72b6df
alltrue6 <55586197+alltrue6@users.noreply.github.com>
2022-10-28 14:31:57 +0800

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -17,13 +17,5 @@ const toButton = () => {
 </template>
 
 <style scoped lang="scss">
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.5s ease;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-    opacity: 0;
-}
 </style>
